19a35147cbc5a3a05f836e5538552a25563816bf
Hugo Junior <hjunioreng@gmail.com>
2023-03-08 23:34:40 -0300

    feat create getById

--- src/app.js
+++ src/app.js
@@ -11,8 +11,9 @@ app.use(cors());
 import authRouter from './routes/auth.router.js';
 import postsRouter from './routes/posts.router.js';
 import trendingRoutes from './routes/trending.routes.js';
+import userPage from './routes/userPage.router.js';
 
-app.use ([authRouter, postsRouter, trendingRoutes]);
+app.use ([authRouter, postsRouter, trendingRoutes, userPage]);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port ${port}.`));
