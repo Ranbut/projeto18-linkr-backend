d279156b72380aee5445dc2afc0d1ef3a3fbc62e
Ranbut <Ranbut_Beater@protonmail.com>
2023-03-09 08:22:36 -0300

    fix: calling by 'username' insted of 'userName'

--- src/routes/userPage.router.js
+++ src/routes/userPage.router.js
@@ -1,11 +1,10 @@
 import {Router} from 'express'
-import {filterUsers, getByIdUserPage} from '../controllers/userPage.controller.js'
-
+import {filterUsers, getByIdUserPage, GetUserByToken} from '../controllers/userPage.controller.js'
 
 const userPage = Router()
 
-
 userPage.get("/user/:username", filterUsers)
 userPage.get("/user/:id/post", getByIdUserPage)
+userPage.post("/get-user", GetUserByToken);
 
 export default userPage
