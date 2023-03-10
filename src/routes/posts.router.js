5afa8e3bc1e7d6261d66cd26711ebab387d76f4a
drebraga <dre.braga@gmail.com>
2023-03-10 14:51:17 -0300

    feat: delete post funtion added

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getPost, pushPost, editPost } from '../controllers/posts.controller.js';
+import { getPost, pushPost, editPost, deletePost } from '../controllers/posts.controller.js';
 import getHashtag from '../middlewares/getHashtag.middleware.js';
 import { postPushValidation } from '../middlewares/posts.middleware.js';
 import { authValidation } from '../middlewares/generics.middleware.js';
@@ -9,5 +9,6 @@ const postsRouter = Router();
 postsRouter.get("/posts", getPost);
 postsRouter.post("/posts", postPushValidation, getHashtag(), pushPost);
 postsRouter.put("/posts/:id", authValidation, editPost);
+postsRouter.delete("/posts/delete/:id", deletePost);
 
 export default postsRouter;
