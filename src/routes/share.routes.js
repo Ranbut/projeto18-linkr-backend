fc286128c83696cc0a6222cd094799f7c9f995a4
drebraga <dre.braga@gmail.com>
2023-03-24 00:34:20 -0300

    fix: small fixes in share controller

--- src/routes/share.routes.js
+++ src/routes/share.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getShareList, sharePost } from '../controllers/share.controller.js';
+import { authValidation } from '../middlewares/generics.middleware.js';
 
 const shareRouter = Router();
 
-shareRouter.get("/share", getShareList)
-shareRouter.post("/share", sharePost);
+shareRouter.get("/share/:id", authValidation, getShareList);
+shareRouter.post("/share/:id", authValidation, sharePost);
 
 export default shareRouter;
